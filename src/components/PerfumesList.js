120d3d86dadbc74dc66aac5d6d4ec2843732300d
RivaD2 <rivkadavidowski@gmail.com>
2021-01-29 11:28:26 -0800

    added perfumeCard component and css

---
+++ src/components/PerfumesList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PerfumeCard from './PerfumeCard';
+
+const PerfumesList = () => {
+  return (
+    <div>
+      <PerfumeCard />
+    </div>
+  )
+}
+export default PerfumesList;
