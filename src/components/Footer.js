8cae91623949ae7b059293900ebaf9741e06fde2
Riva Davidowski <rivkadavidowski@gmail.com>
2021-01-31 15:57:00 -0800

    added Link to connec-block icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import IconButton from "@material-ui/core/IconButton";
 import TwitterIcon from '@material-ui/icons/Twitter';
 import PinterestIcon from '@material-ui/icons/Pinterest';
+import {Link} from 'react-router-dom';
 import '../components/Footer.css';
 
 export default class Footer extends React.Component {
@@ -12,19 +13,34 @@ export default class Footer extends React.Component {
       <div className="footer-container"> 
         <div className="connect-block">
           <div className="connect-icons">
-
-            <IconButton>
-              <InstagramIcon/>
-            </IconButton>
-            <IconButton>
-              <FacebookIcon />
-            </IconButton>
-            <IconButton>
-              <TwitterIcon />
-            </IconButton>
-            <IconButton>
-              <PinterestIcon/>
-            </IconButton>
+          <Link 
+            to={{pathname:"https://www.instagram.com/?hl=en"}}
+            target="#">
+              <IconButton>
+                <InstagramIcon/>
+              </IconButton>
+          </Link>
+          <Link 
+            to={{pathname:"https://www.facebook.com/"}}
+            target="#">
+              <IconButton>
+                <FacebookIcon />
+              </IconButton>
+          </Link>
+          <Link
+            to={{pathname:"https://twitter.com/?lang=en"}}
+            target="#">
+              <IconButton>
+                <TwitterIcon />
+              </IconButton>
+          </Link>
+          <Link
+            to={{pathname:"https://www.pinterest.com/"}}
+            target="#">
+              <IconButton>
+                <PinterestIcon/>
+              </IconButton>
+          </Link>
           </div>
         </div>
         <div className="footer-link">
