1e120d89b5906c93fd3892ce922a387ee8280e75
RivaD2 <rivkadavidowski@gmail.com>
2021-02-01 15:58:39 -0800

    flex for carousel and perfume links

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,15 +2,18 @@ import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import PerfumeCard from './PerfumeCard';
+import './Carousel.css';
 
 class PerfumeCarousel extends React.Component {
     render() {
         let perfumeCards = this.props.cards;
         const listOfPerfumes = perfumeCards.map(card => <PerfumeCard key={card}/>)
         return (
-            <Carousel>
-              {listOfPerfumes}
-            </Carousel>
+            <div className="carousel-container">
+              <Carousel>
+                {listOfPerfumes}
+              </Carousel>
+            </div>
         );
     }
 };
--- src/components/PerfumesList.js
+++ src/components/PerfumesList.js
@@ -7,9 +7,12 @@ export default class PerfumesList extends React.Component {
     const tempCards = [{},{},{},{},{}];
     return (
       <div className="perfume-container">
+          <div className="perfumes-links">
+            AQABA FEMININE/AQABA MASCULINE
+          </div>
         <div className="perfume-cards">
-        </div>
           <PerfumeCarousel cards={tempCards}/>
+          </div>
       </div>
     )
   }
