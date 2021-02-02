d06335655e03b0a08b8775f4a7ff4abfa910efda
RivaD2 <rivkadavidowski@gmail.com>
2021-02-01 18:46:36 -0800

    created perfume links masculine/feminine

--- src/components/PerfumesList.js
+++ src/components/PerfumesList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PerfumeCarousel from './Carousel';
+import {Link} from 'react-router-dom';
 import './PerfumesList.css';
 
 export default class PerfumesList extends React.Component {
@@ -7,8 +8,13 @@ export default class PerfumesList extends React.Component {
     const tempCards = [{},{},{},{},{}];
     return (
       <div className="perfume-container">
-          <div className="perfumes-links">
-            AQABA FEMININE/AQABA MASCULINE
+          <div className="perfumes-links-container">
+            <Link to="/perfumes-masculine" className="perfume-links">
+              AQABA FEMININE /
+            </Link>
+            <Link to="/perfumes-masculine" className="perfume-links">
+              AQABA MASCULINE
+            </Link>
           </div>
         <div className="perfume-cards">
           <PerfumeCarousel cards={tempCards}/>
