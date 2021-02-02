40d69196c4f4198d116af90d25073e6d9934d393
RivaD2 <rivkadavidowski@gmail.com>
2021-02-02 11:16:50 -0800

    perfumes page layout done

--- src/components/PerfumesList.js
+++ src/components/PerfumesList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PerfumeCarousel from './Carousel';
+import PerfumeCard from './PerfumeCard';
 import {Link} from 'react-router-dom';
 import './PerfumesList.css';
 
@@ -8,6 +9,16 @@ export default class PerfumesList extends React.Component {
     const tempCards = [{},{},{},{},{}];
     return (
       <div className="perfume-container">
+        <div className="most-popular">
+          MOST POPULAR
+        </div>
+        <div className="perfume-cards">
+          {/* Replace cards in Carousel with full size images, no effects */}
+          <PerfumeCarousel cards={tempCards}/>
+          </div>
+          <div>
+            <hr />
+          </div>
           <div className="perfumes-links-container">
             <Link to="/perfumes-masculine" className="perfume-links">
               AQABA FEMININE /
@@ -15,9 +26,12 @@ export default class PerfumesList extends React.Component {
             <Link to="/perfumes-masculine" className="perfume-links">
               AQABA MASCULINE
             </Link>
-          </div>
-        <div className="perfume-cards">
-          <PerfumeCarousel cards={tempCards}/>
+              <div className="card-container">
+               {/* Grid for Card layout */}
+               <div className="card">
+               <PerfumeCard />
+               </div>
+              </div>
           </div>
       </div>
     )
