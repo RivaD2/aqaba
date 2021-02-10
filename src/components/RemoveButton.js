a58f63d7739cbefcb28d1c93bcb5c4c3c7a2478e
RivaD2 <rivkadavidowski@gmail.com>
2021-02-10 10:50:29 -0800

    created RemoveButton Component

---
+++ src/components/RemoveButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+const IconLabelButtons = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        startIcon={<DeleteIcon />}
+      >
+        Delete
+      </Button>
+    </div>
