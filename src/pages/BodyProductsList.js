a5df7e2fa91de31195ef42cd1f32a3c38b756c3d
RivaD2 <rivkadavidowski@gmail.com>
2021-03-17 13:15:28 -0700

    rendered list of body products

--- src/pages/BodyProductsList.js
+++ src/pages/BodyProductsList.js
@@ -1,31 +1,50 @@
 import React from 'react'
 import {getPerfumes} from '../Axios'
+import PerfumeCard from '../components/PerfumeCard';
 
 export default class BodyProductsList extends React.Component {
   state = {
-    list: undefined
+    productsList: undefined,
+    filterByCategory: 'body'
   }
 
   async componentDidMount() {
     const listOfProducts = await getPerfumes();
-    let bodyProducts = [];
-    listOfProducts.filter((object => {
-      if(object.category === "body"){
-        bodyProducts.push(object)
-      }
-      return bodyProducts;
-    }))
-
-    console.log('getting list of body products', bodyProducts)
     this.setState({
-      bodyProducts
+      listOfProducts
+    });
+  }
+   
+  onListFilterBodyProducts = category => {
+    this.setState({
+      filterByCategory: category
     })
   }
   
   render() {
+    const {listOfProducts} = this.state;
+    const bodyProductCards = listOfProducts === undefined ? [] : listOfProducts.filter(card => {
+  return card.category === this.state.filterByCategory
+    })
     return (
-      <div className="gifts-container">
-        BATH and BODY
+      <div className="body-products-container">
+          <div className="links-container">
+            <div className="body-links" onClick={() => this.onListFilterBodyProducts('body')}>
+              BATH 
+            </div>
+            <div className="body-links" onClick={() => this.onListFilterBodyProducts('bath')}>
+              BODY
+            </div>
+          </div>
+          <div className="card-container">
+            <div className="cards">
+              {bodyProductCards.map(card => (
+                <div classname="body-card" key={card._id}>
+                  <PerfumeCard product={card}/>
+                </div>
+              ))}
+            </div>
+          </div>
       </div>
     )
   }
