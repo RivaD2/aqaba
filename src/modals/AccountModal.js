5dd82a41b3431279c79cda53fc674a8ceb606f19
RivaD2 <rivkadavidowski@gmail.com>
2021-04-22 11:30:41 -0700

    AccountModal styling complete

--- src/modals/AccountModal.js
+++ src/modals/AccountModal.js
@@ -6,18 +6,29 @@ import './AccountModal.css'
 const AccountModal = props => {
   return (
     <div className="modal-container">
-        <div className="modal-content">
-         <div className="modal-header">
-           Welcome to Aqaba
-         </div>
-         <div 
-          className="modal-image-container" 
-          style={{backgroundImage:`url('${process.env.PUBLIC_URL}/assets/aqaba.amour.png')`}} 
-         />
-         <TextField field={{label: 'Username or email'}}> Enter Username or Email</TextField>
-         <TextField field={{label: 'Enter Password'}}> Enter Password </TextField>
-         <TextButton text='CREATE ACCOUNT' onClick={props.close}/>
+      <div className="modal-content">
+        <div className="modal-header">
+          Welcome To AQABA
         </div>
+        <div className="header-slogan-text">A CELEBRATION OF LOVE</div>
+        <div className="modal-image-container">
+          <div className="account-modal-image">
+            <img 
+              src="/assets/aqaba-amour.png" 
+              alt="aqaba-classic"
+              className="aqaba-product-image"
+            />
+          </div>
+          <div className="create-account-header">
+            CREATE ACCOUNT
+        </div>
+        </div>
+        <TextField field={{label: 'Username or email'}} className="form-text" />
+        <TextField field={{label: 'Enter Password'}} className="form-text" />
+        <div className="create-account-button">
+          <TextButton text='CREATE ACCOUNT' onClick={props.close}/>
+        </div>
+      </div>
     </div>
   )
 }
