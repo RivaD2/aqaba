fe5bc06494d8f15f9fab584878d870f0fc0122c8
RivaD2 <rivkadavidowski@gmail.com>
2021-05-08 14:39:12 -0700

    media query for mobile footer and App

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,51 +11,70 @@ export default class Footer extends React.Component {
   render() {
     return (
       <div className="footer-container"> 
-        <div className="connect-block">
-          <div className="connect-icons">
-          <Link 
-            to={{pathname:"https://www.instagram.com/?hl=en"}}
-            target="#">
-              <IconButton>
-                <InstagramIcon/>
-              </IconButton>
-          </Link>
-          <Link 
-            to={{pathname:"https://www.facebook.com/"}}
-            target="#">
-              <IconButton>
-                <FacebookIcon />
-              </IconButton>
-          </Link>
-          <Link
-            to={{pathname:"https://twitter.com/?lang=en"}}
-            target="#">
-              <IconButton>
-                <TwitterIcon />
-              </IconButton>
-          </Link>
-          <Link
-            to={{pathname:"https://www.pinterest.com/"}}
-            target="#">
-              <IconButton>
-                <PinterestIcon/>
-              </IconButton>
-          </Link>
+        <div className="aqaba-summary-container">
+          <div className="aqaba-summary">
+            <p>ABOUT AQABA:</p>
+            <p>The Aqaba perfume line was founded in New York and brings
+              to you inspired scents from this small city by the Red Sea in Jordan.</p>
           </div>
         </div>
-        <div className="footer-link">
-          PRESS
+        <div className="icon-and-link-container">
+          <div className="connect-block">
+            <div className="connect-icons">
+            <Link 
+              to={{pathname:"https://www.instagram.com/?hl=en"}}
+              target="#">
+                <IconButton>
+                  <InstagramIcon/>
+                </IconButton>
+            </Link>
+            <Link 
+              to={{pathname:"https://www.facebook.com/"}}
+              target="#">
+                <IconButton>
+                  <FacebookIcon />
+                </IconButton>
+            </Link>
+            <Link
+              to={{pathname:"https://twitter.com/?lang=en"}}
+              target="#">
+                <IconButton>
+                  <TwitterIcon />
+                </IconButton>
+            </Link>
+            <Link
+              to={{pathname:"https://www.pinterest.com/"}}
+              target="#">
+                <IconButton>
+                  <PinterestIcon/>
+                </IconButton>
+            </Link>
+            </div>
+          </div>
+          <div className="footer-link">
+            PRESS
+          </div>
+          <div className="footer-link">
+            BLOG
+          </div>
+          <div className="footer-link">
+            VIDEO
+          </div>
+          <div className="footer-link">
+            SUBSCRIBE
+          </div>
         </div>
-        <div className="footer-link">
-          BLOG
+        <div className="form-container">
+          <p className="form-title">SUBSCRIBE</p>
+          <form id="email-form">
+            <div className="form-group">
+              <label htmlFor="email-address"/>
+              <input type="text" placeholder="Enter your email address" className="form-control" />
+            </div>
+          </form>
+          <button>SUBSCRIBE</button>
         </div>
-        <div className="footer-link">
-          VIDEO
-        </div>
-        <div className="footer-link">
-          SUBSCRIBE
-        </div>
-      </div>
+     </div>
     )
   }
 }
