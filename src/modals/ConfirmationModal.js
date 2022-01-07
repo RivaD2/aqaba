453ed243e917fe0f677a2a36f24117ac9f2aa7b1
RivaD2 <rivkadavidowski@gmail.com>
2022-01-07 11:08:34 -0800

    semantic tags and code cleanup ConfirmationModal

--- src/modals/ConfirmationModal.js
+++ src/modals/ConfirmationModal.js
@@ -1,66 +1,48 @@
-import React from 'react'
-import './ConfirmationModal.css';
+import React from "react";
+import "./ConfirmationModal.css";
 
 /**
  * Function that renders Confirmation Modal
  */
-const ConfirmationModal = props => {
+const ConfirmationModal = (props) => {
   return (
     <div className="order-confirmation-container">
-        <div className="title">
-          AQABA
-        </div>
-        <hr />
-        <div className="confirmation-header">
-          Thank you for your order!
-        </div>
-        <div className="order-number">
-         Order Number:  #C442879
-        </div>
-        <div className="tracking-text"> We'll send you tracking information when the order ships.</div>
-      <div className="cart-count">
-          Your Items: ({props.cart.qty})
-      </div>
+      <h1 className="title">AQABA</h1>
+      <hr />
+      <p className="confirmation-header">Thank you for your order!</p>
+      <p className="order-number">Order Number: #C442879</p>
+      <p className="tracking-text">
+        {" "}
+        We'll send you tracking information when the order ships.
+      </p>
+      <p className="cart-count">Your Items: ({props.cart.qty})</p>
       <div className="items-from-cart">
-        {props.cart.items && props.cart.items.map((item, index) => (
-          <div className="perfume-card" key={index}>
-            <div className="cart-image">
-              <img 
-                src={`/assets/${item.image}`} 
-                className="aqaba-image" 
-                alt="product">
-              </img>
-            </div>
-            <div className="cart-details">
-                <div className="product-title">
-                {item.title}
+        {props.cart.items &&
+          props.cart.items.map((item, index) => (
+            <div className="perfume-card" key={index}>
+              <div className="cart-image">
+                <img
+                  src={`/assets/${item.image}`}
+                  className="aqaba-image"
+                  alt="product"
+                ></img>
               </div>
-              <div className="product-price">
-                Price: ${item.price} USD
-              </div>
-              <div className="product-size-selected">
-                Size: {item.size}
-              </div>
-              <div className="product-qty">
-                Qty:{item.qty}
+              <div className="cart-details">
+                <p className="product-title">{item.title}</p>
+                <p className="product-price">Price: ${item.price} USD</p>
+                <p className="product-size-selected">Size: {item.size}</p>
+                <p className="product-qty">Qty:{item.qty}</p>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
         <hr />
-         <div className="totals">
-           <div className="payment-method">
-             Payment Method: Credit Card
-           </div>
-            <div className="total-shipping">
-              Shipping: FREE
-            </div>
-            <div className="order-total">
-              Total: ${props.cart.price}
-            </div>  
-          </div>
+        <div className="totals">
+          <p className="payment-method">Payment Method: Credit Card</p>
+          <p className="total-shipping">Shipping: FREE</p>
+          <p className="order-total">Total: ${props.cart.price}</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default ConfirmationModal;
