ae75a55b1352326573adde04bfe5a9aabdfa7d94
RivaD2 <rivkadavidowski@gmail.com>
2022-01-30 17:21:14 -0800

    footer and header tags addition and edits layout icon block

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,65 +1,60 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
+import React from "react";
+import { Link } from "react-router-dom";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
 import IconButton from "@material-ui/core/IconButton";
-import TwitterIcon from '@material-ui/icons/Twitter';
-import PinterestIcon from '@material-ui/icons/Pinterest';
-import '../components/Footer.css';
+import TwitterIcon from "@material-ui/icons/Twitter";
+import PinterestIcon from "@material-ui/icons/Pinterest";
+import "../components/Footer.css";
 
 export default class Footer extends React.Component {
   render() {
     return (
-      <div className="footer-container"> 
+      <footer className="footer-container">
         <div className="aqaba-summary-container">
           <div className="aqaba-summary">
             <p className="mobile-summary-title">ABOUT AQABA:</p>
-            <p>The Aqaba perfume line was founded in New York and brings
-              to you inspired scents from this small city by the Red Sea in Jordan.</p>
+            <p>
+              House of Appa is inspired by Avatar: The Last Airbender, an
+              American animated television series. Scents are not for purchase
+              and are placeholders for demo purposes.
+            </p>
           </div>
         </div>
         <div className="icon-and-link-container">
           <div className="connect-block">
             <div className="connect-icons">
-            <Link
-              to={{pathname:"https://www.instagram.com/?hl=en"}}
-              target="#">
+              <Link
+                to={{ pathname: "https://www.instagram.com/?hl=en" }}
+                target="#"
+              >
                 <IconButton>
-                  <InstagramIcon/>
+                  <InstagramIcon />
                 </IconButton>
-            </Link>
-            <Link
-              to={{pathname:"https://www.facebook.com/"}}
-              target="#">
+              </Link>
+              <Link to={{ pathname: "https://www.facebook.com/" }} target="#">
                 <IconButton>
                   <FacebookIcon />
                 </IconButton>
-            </Link>
-            <Link
-              to={{pathname:"https://twitter.com/?lang=en"}}
-              target="#">
+              </Link>
+              <Link
+                to={{ pathname: "https://twitter.com/?lang=en" }}
+                target="#"
+              >
                 <IconButton>
                   <TwitterIcon />
                 </IconButton>
-            </Link>
-            <Link
-              to={{pathname:"https://www.pinterest.com/"}}
-              target="#">
+              </Link>
+              <Link to={{ pathname: "https://www.pinterest.com/" }} target="#">
                 <IconButton>
-                  <PinterestIcon/>
+                  <PinterestIcon />
                 </IconButton>
-            </Link>
+              </Link>
             </div>
           </div>
-          <div className="footer-link">
-            PRESS
-          </div>
-          <div className="footer-link">
-            BLOG
-          </div>
-          <div className="footer-link">
-            VIDEO
-          </div>
+          <div className="footer-link">PRESS</div>
+          <div className="footer-link">BLOG</div>
+          <div className="footer-link">VIDEO</div>
           <div className="footer-link" onClick={this.props.showSubscribeModal}>
             SUBSCRIBE
           </div>
@@ -68,13 +63,22 @@ export default class Footer extends React.Component {
           <p className="form-title">SUBSCRIBE</p>
           <form id="email-form">
             <div className="form-group">
-              <label htmlFor="email-address"/>
-              <input type="text" placeholder="Enter your email address" className="form-control" />
+              <label htmlFor="email-address" />
+              <input
+                type="text"
+                placeholder="Enter your email address"
+                className="form-control"
+              />
             </div>
           </form>
-          <button className="subscribe-button" onClick={this.props.showSubscribeModal}>SUBSCRIBE</button>
+          <button
+            className="subscribe-button"
+            onClick={this.props.showSubscribeModal}
+          >
+            SUBSCRIBE
+          </button>
         </div>
-     </div>
-    )
+      </footer>
+    );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,78 +1,83 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom';
-import Nav from '../components/Nav';
-import AddShoppingCartOutlinedIcon from '@material-ui/icons/AddShoppingCartOutlined';
-import SearchBar from '../components/SearchBar';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Nav from "../components/Nav";
+import AddShoppingCartOutlinedIcon from "@material-ui/icons/AddShoppingCartOutlined";
+import SearchBar from "../components/SearchBar";
 import SearchOutinedIcon from "@material-ui/icons/SearchOutlined";
-import SideBar from '../components/SideBar';
+import SideBar from "../components/SideBar";
 import IconButton from "@material-ui/core/IconButton";
-import Login from '../components/Login';
-import './Header.css';
+import Login from "../components/Login";
+import "./Header.css";
 
 class Header extends React.Component {
   state = {
     showSearchField: false,
   };
-  
+
   /**
    * Toggles state of SearchField component
    */
   toggleSearchField = () => {
-    if(this.state.searchTerm) {
+    if (this.state.searchTerm) {
       return;
     }
     this.setState({
-      showSearchField: !this.state.showSearchField
+      showSearchField: !this.state.showSearchField,
     });
-  }
-  
+  };
+
   /**
    * Sets new searchterm entered into input into state
    * @param {string} searchTerm
    */
-  onSearchChanged = searchTerm => {
+  onSearchChanged = (searchTerm) => {
     this.setState({
-     searchTerm
+      searchTerm,
     });
-  }
+  };
 
   render() {
     return (
-      <div className="header">
+      <header className="header">
         <div className="nav-container">
           <div className="hamburger">
-            <SideBar/>
+            <SideBar />
           </div>
           <div className="title-offset"></div>
-          <div className="title-block">
-            <NavLink to="/" className="home-title"> HOUSE OF APPA</NavLink>
-            <div className="link-block" >
+          <h1 className="title-block">
+            <NavLink to="/" className="home-title">
+              {" "}
+              HOUSE OF APPA
+            </NavLink>
+            <div className="link-block">
               <Nav navLinkClass="nav-item" />
             </div>
-          </div>
+          </h1>
           <div className="icon-block">
             <div className="search-icon">
-                <IconButton onClick={this.toggleSearchField}> 
-                  <SearchOutinedIcon />
-                </IconButton>
+              <IconButton onClick={this.toggleSearchField}>
+                <SearchOutinedIcon />
+              </IconButton>
+            </div>
+            <div className="cart-icon">
+              <IconButton onClick={this.props.showCart}>
+                <AddShoppingCartOutlinedIcon />
+              </IconButton>
+            </div>
+            <div className="account-icon">
+              <div className="login-icon-container">
+                <Login showLoginModal={this.props.showLoginModal} />
               </div>
-              <div className="cart-icon">
-                <IconButton onClick={this.props.showCart}>
-                  <AddShoppingCartOutlinedIcon />
-                </IconButton>
-              </div>
-              <div className="account-icon">
-                <div className="login-icon-container">
-                  <Login showLoginModal={this.props.showLoginModal}/>
-                </div>
-              </div>
+            </div>
           </div>
         </div>
         <div className="search-container">
-          {this.state.showSearchField && <SearchBar onSearchChanged={this.onSearchChanged}/>}
+          {this.state.showSearchField && (
+            <SearchBar onSearchChanged={this.onSearchChanged} />
+          )}
         </div>
-      </div>
-    )
+      </header>
+    );
   }
 }
 
