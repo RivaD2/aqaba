f782082416c09e6ed2b91f8af5eaf00ea9720c73
RivaD2 <rivkadavidowski@gmail.com>
2022-04-03 13:34:57 -0700

    hero objects and props to Hero component, background images addition

--- src/pages/AqabaHome.js
+++ src/pages/AqabaHome.js
@@ -1,22 +1,31 @@
 import React from 'react'
-import { Hidden } from '@material-ui/core';
-import {Link} from 'react-router-dom';
-import ImageFade from '../components/ImageFade';
-import TextButton from '../components/TextButton';
+// import ImageFade from '../components/ImageFade';
+import Hero from '../components/Hero';
 import '../pages/AqabaHome.css';
 
 const AqabaHome = props  => {
+  const hero1 = {
+    backgroundImg: "assets/avatar-hero-1.jpg",
+    perfumeImg: "assets/appa-in-the-flowerbed.png",
+    altText: "Appa and Angflying over multicolored flowerbed",
+    heroText: "Air-bending fragrances",
+    buttonText :"Discover Air",
+    buttonLink :"/perfumes/air",
+  }
+
+  // const hero2 = {
+  //   backgroundImg: "assets/hero-2.jpg",
+  //   perfumeImg: "assets/earth-kingdom.jpg",
+  //   altText:"green bottle filled with two lovers sitting next to one another",
+  //   heroText: "Find your Avatar State",
+  //   buttonText :"Discover Earth",
+  //   buttonLink :"/perfumes/earth",
+  // }
+
   return (
     <div className="home App-body">
-        <ImageFade />
-      <div className="home-content">
-        <Link to="/perfumes" style={{ textDecoration: 'inherit'}} className="perfume-link">
-          <Hidden xsDown>
-            <TextButton text='Discover Aqaba' />
-          </Hidden>
-          <button className="home-mobile-button">DISCOVER AQABA</button>
-        </Link>
-      </div>
+        {/* <ImageFade /> */}
+        <Hero {...hero1} />
     </div>
   )
 }
