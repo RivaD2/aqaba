17137a01fedb5b0617eb7f7f67d89e6caa4cf874
RivaD2 <rivkadavidowski@gmail.com>
2022-04-09 13:47:23 -0700

    Turning hero objects to array of objects for map

--- src/pages/AqabaHome.js
+++ src/pages/AqabaHome.js
@@ -4,51 +4,51 @@ import Hero from '../components/Hero';
 import '../pages/AqabaHome.css';
 
 const AqabaHome = props => {
-  const hero1 = {
-    backgroundImg: "assets/avatar-hero-1.png",
-    perfumeImg: "assets/appa-in-the-flowerbed.png",
-    altText: "Appa and Ang flying over multicolored flowerbed",
-    heroText: "Air-bending fragrances",
-    buttonText :"Discover Perfumes",
-    buttonLink :"/perfumes/air",
-  }
-
-  const hero2 = {
-    backgroundImg: "assets/hero-2.png",
-    perfumeImg: "assets/momo.png",
-    altText:"orange and turquoise perfume bottle showing surfer and fish below",
-    heroText: "Find your Avatar State",
-    buttonText :"Discover Perfumes",
-    buttonLink :"/perfumes",
-    alignRight: true
-  }
-
-  const hero3 = {
-    backgroundImg: "assets/hero-3.png",
-    perfumeImg: "assets/water-kingdom.png",
-    altText:"blue perfume bottle with bird coming out of a lemon",
-    heroText: "Way of The Water Bender",
-    buttonText :"Discover Perfumes",
-    buttonLink :"/perfumes",
-  }
-
-  const hero4 = {
-    backgroundImg: "assets/hero-4.png",
-    perfumeImg: "assets/earth-kingdom.png",
-    altText:"green bottle filled with two lovers sitting next to one another",
-    heroText: "Down To Earth Vibes",
-    buttonText :"Discover Earth",
-    buttonLink :"/perfumes",
-    alignRight: true
-  }
-
+  const heroBackgrounds = [
+   {
+      backgroundImg: "assets/avatar-hero-1.png",
+      perfumeImg: "assets/appa-in-the-flowerbed.png",
+      altText: "Appa and Ang flying over multicolored flowerbed",
+      heroText: "Air-bending fragrances",
+      buttonText :"Discover Perfumes",
+      buttonLink :"/perfumes/air",
+    },
+    {
+      backgroundImg: "assets/hero-2.png",
+      perfumeImg: "assets/momo.png",
+      altText:"orange and turquoise perfume bottle showing surfer and fish below",
+      heroText: "Find your Avatar State",
+      buttonText :"Discover Perfumes",
+      buttonLink :"/perfumes",
+      alignRight: true
+    },
+    {
+      backgroundImg: "assets/hero-3.png",
+      perfumeImg: "assets/water-kingdom.png",
+      altText:"blue perfume bottle with bird coming out of a lemon",
+      heroText: "Way of The Water Bender",
+      buttonText :"Discover Perfumes",
+      buttonLink :"/perfumes",
+    },
+    {
+      backgroundImg: "assets/hero-4.png",
+      perfumeImg: "assets/earth-kingdom.png",
+      altText:"green bottle filled with two lovers sitting next to one another",
+      heroText: "Down To Earth Vibes",
+      buttonText :"Discover Earth",
+      buttonLink :"/perfumes",
+      alignRight: true
+    }
+  ]
+  const heroBackgroundsForImageFade = heroBackgrounds.map(heroObject => (
+     <Hero key={heroObject.altText}
+       {...heroObject}
+      />
+  ))
   return (
     <div className="home App-body">
         <ImageFade>
-          <Hero {...hero1}></Hero>
-          <Hero {...hero2}></Hero>
-          <Hero {...hero3}></Hero>
-          <Hero {...hero4}></Hero>
+          {heroBackgroundsForImageFade}
         </ImageFade>
     </div>
   )
